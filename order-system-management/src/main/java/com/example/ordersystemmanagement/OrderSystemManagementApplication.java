50bab3d5ba3e669f628fd6d51f8af8dc8420ef19
rizaozer <rizaoozer@gmail.com>
2022-09-08 22:56:32 +0300

    initial commit

---
+++ order-system-management/src/main/java/com/example/ordersystemmanagement/OrderSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordersystemmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSystemManagementApplication.class, args);
+    }
+
+}
---
+++ order-system-management/src/test/java/com/example/ordersystemmanagement/OrderSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordersystemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
