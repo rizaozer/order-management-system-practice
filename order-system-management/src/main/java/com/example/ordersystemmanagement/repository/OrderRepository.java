5c369610b89c4672ae858006cd99b74de14753ca
rizaozer <rizaoozer@gmail.com>
2022-09-14 00:04:53 +0300

    add search methods

--- order-system-management/src/main/java/com/example/ordersystemmanagement/repository/OrderRepository.java
+++ order-system-management/src/main/java/com/example/ordersystemmanagement/repository/OrderRepository.java
@@ -2,9 +2,21 @@ package com.example.ordersystemmanagement.repository;
 
 import com.example.ordersystemmanagement.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByCreationDate(LocalDate creationDate);
 
+    @Query("SELECT o FROM Order o WHERE o.customer.fullName = :name")
+    List<Order> findAllByCustomerName(String name);
+
+    @Query("SELECT o FROM Order o JOIN o.orderLines ol WHERE ol.product.name = :productName")
+    List<Order> findAllByProductName(String productName);
+
+    @Query("SELECT o FROM Order o JOIN o.orderLines ol WHERE ol.product.skuCode = :skuCode")
+    List<Order> findAllByProductSkuCode(long skuCode);
 }
