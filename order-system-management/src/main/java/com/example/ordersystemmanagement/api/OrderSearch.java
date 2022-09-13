0e50118eb87d3526c321d1b9aa522e5bd738acf4
rizaozer <rizaoozer@gmail.com>
2022-09-14 00:15:32 +0300

    add OrderSearch class

--- order-system-management/src/main/java/com/example/ordersystemmanagement/api/OrderSearch.java
+++ order-system-management/src/main/java/com/example/ordersystemmanagement/api/OrderSearch.java
@@ -1,2 +1,35 @@
-package com.example.ordersystemmanagement.api;public class OrderSearch {
+package com.example.ordersystemmanagement.api;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class OrderSearch {
+
+    private String key;
+    private String value;
+
+    public OrderSearchParam getKey() {
+        return OrderSearchParam.from(key);
+    }
+
+    public String getStringValue() {
+        return Objects.toString(value);
+    }
+
+    public Integer getIntegerValue() {
+        try {
+            return Integer.parseInt(getStringValue());
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public LocalDate getDateValue() {
+        return LocalDate.parse(getStringValue());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
 }
