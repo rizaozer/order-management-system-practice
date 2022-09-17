881dfca82e301a5983857ce089b459f3ab20809c
rizaozer <rizaoozer@gmail.com>
2022-09-17 21:33:46 +0300

    add first test

--- order-system-management/src/test/java/com/example/ordersystemmanagement/OrderSystemManagementApplicationTests.java
+++ order-system-management/src/test/java/com/example/ordersystemmanagement/OrderSystemManagementApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.ordersystemmanagement;
 
+import com.example.ordersystemmanagement.controller.OrderController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class OrderSystemManagementApplicationTests {
 
+    @Autowired
+    private OrderController orderController;
+
     @Test
     void contextLoads() {
+        assertThat(orderController).isNotNull();
     }
 
 }
