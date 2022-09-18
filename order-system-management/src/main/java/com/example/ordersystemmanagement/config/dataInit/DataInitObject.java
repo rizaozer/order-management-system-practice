8fd0ba6d01ba3eaf0a6b0b652886839e42aeadc4
rizaozer <rizaoozer@gmail.com>
2022-09-18 13:02:35 +0300

    create DataInitObject

--- order-system-management/src/main/java/com/example/ordersystemmanagement/config/dataInit/DataInitObject.java
+++ order-system-management/src/main/java/com/example/ordersystemmanagement/config/dataInit/DataInitObject.java
@@ -1,2 +1,15 @@
-package com.example.ordersystemmanagement.config.dataInit;public class DataInitObject {
+package com.example.ordersystemmanagement.config.dataInit;
+
+import com.example.ordersystemmanagement.entity.Customer;
+import com.example.ordersystemmanagement.entity.Order;
+import com.example.ordersystemmanagement.entity.Product;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class DataInitObject {
+
+    private List<Customer> customers;
+    private List<Order> orders;
+    private List<Product> products;
 }
